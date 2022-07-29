ca395f7f55b430e07e9125eccd611bc4139552eb
shahin <shahin.shahi1986@gmail.com>
2022-07-29 09:52:48 +0300

    make components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import ScrollToTop from "./components/ScrollToTop";
+import Signup from "./components/Signup";
+import SuperRare from "./components/SuperRare";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Free from "./components/Free";
+import Clients from "./components/Clients";
+import Release from "./components/Release";
+import Like from "./components/Like";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="app-container">
+    <ScrollToTop/>
+    <Navbar/>
+    <Home/>
+    <Free/>
+    <Clients/>
+    <SuperRare/>
+    <Release/>
+    <Like/>
+    <Signup/>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/SuperRare.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SuperRare() {
+  return (
+    <div>SuperRare</div>
+  )
+}
